b110184e13dd5c8e075c5eb43a8bb952e03d5ae4
programmer-anvar <anvartojiboyev733@gmail.com>
2025-12-08 22:49:48 +0500

    feature: install need library

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePage = () => {
+  return (
+    <div>
+      Create
+    </div>
+  )
+}
+
+export default CreatePage
---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoteDetailPage = () => {
+  return (
+    <div>
+      Detail
+    </div>
+  )
+}
+
+export default NoteDetailPage
